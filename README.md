99181950fba922fa54352a243ac810ffb88310f9
Dairon Hernandez <daironh@gmail.com>
2018-07-29 09:42:01 -0400

    firt commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# backend-ninja
+CRUD example in Spring Boot
+
+An example for create a CRUD app for course entity
+
+-Spring DATA Jpa
+
+-Validations
+
+-Twitter Boostrap
+
+-Thymeleaf
---
+++ src/main/java/com/daironh/backendninja/demo/BackendNinjaApplication.java
@@ -0,0 +1,12 @@
+package com.daironh.backendninja.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNinjaApplication.class, args);
+	}
+}
